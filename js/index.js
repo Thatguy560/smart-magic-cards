19e3f2524ed458a9d501d71de7eb48d7e4645cfb
Peter Stevens <Peterstevens510@gmail.com>
2020-10-16 21:32:20 +0100

    suit and cards sorted out

--- js/index.js
+++ js/index.js
@@ -1,22 +1,26 @@
-const suit = 'hearts';
+const suits = ['hearts', 'clubs', 'diamonds', 'spades'];
+const hand = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
 const cardsWrapper = document.querySelector('.cards-wrapper');
 const btnWrapper = document.querySelector('.btn-wrapper'); /* eslint-disable-line */
 const selectedCardsWrapper = document.querySelector('.selected-cards'); /* eslint-disable-line */
 
+const startButton = document.getElementById('start-game');
+
 function createCards() {
   const cards = [];
-  // Create an array with objects containing the value and the suit of each card
-  for (let i = 1; i <= 13; i += 1) {
-    const cardObject = {
-      value: i,
-      suit,
-    };
-    cards.push(cardObject);
+  // Create an array with objects containing the value and the suit of each card (done)
+  for (let s = 0; s < suits.length; s += 1) {
+    for (let c = 1; c <= hand.length; c += 1) {
+      const cardObject = {
+        value: c,
+        suit: suits[s],
+      };
+      cards.push(cardObject);
+    }
   }
-
   // For each dataObject, create a new card and append it to the DOM
   cards.forEach((card, i) => {
-    const positionFromLeft = i * 15;
+    const positionFromLeft = i * 22.5;
     const cardElement = document.createElement('div');
     cardElement.setAttribute('data-value', card.value);
     cardElement.classList.add('card', `${card.suit}-${card.value}`);
@@ -37,4 +41,5 @@ function startGame() {
   createCards();
 }
 
-document.getElementById('start-game').addEventListener('click', startGame);
+// document.getElementById('start-game').addEventListener('click', startGame);
+startButton.addEventListener('click', startGame);
