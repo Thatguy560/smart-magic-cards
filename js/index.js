73fbbc67ea8f8724b9d89998c807d42966c8fad6
Peter Stevens <Peterstevens510@gmail.com>
2020-10-19 15:34:40 +0100

    updated position index and starting to work on buttons

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 const suits = ['hearts', 'clubs', 'diamonds', 'spades'];
 const hand = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
+const cards = [];
+
 const cardsWrapper = document.querySelector('.cards-wrapper');
-const btnWrapper = document.querySelector('.btn-wrapper'); /* eslint-disable-line */
-const selectedCardsWrapper = document.querySelector('.selected-cards'); /* eslint-disable-line */
 
 const startButton = document.getElementById('start-game');
 
+const btnWrapper = document.querySelector('.btn-wrapper'); /* eslint-disable-line */
+const selectedCardsWrapper = document.querySelector('.selected-cards');
+ /* eslint-disable-line */
+
+
+// Create an array with objects containing the value and the suit of each card (done)
 function createCards() {
-  const cards = [];
-  // Create an array with objects containing the value and the suit of each card (done)
   for (let s = 0; s < suits.length; s += 1) {
     for (let c = 1; c <= hand.length; c += 1) {
       const cardObject = {
@@ -20,7 +24,7 @@ function createCards() {
   }
   // For each dataObject, create a new card and append it to the DOM
   cards.forEach((card, i) => {
-    const positionFromLeft = i * 22.5;
+    const positionFromLeft = i * 25; // Card spacing
     const cardElement = document.createElement('div');
     cardElement.setAttribute('data-value', card.value);
     cardElement.classList.add('card', `${card.suit}-${card.value}`);
@@ -29,9 +33,23 @@ function createCards() {
   });
 }
 
+function shuffle() {
+
+}
+
+function flipCards() {
+
+}
+
+function magic() {
+
+}
+
 // Function to clear out the initial button and create new buttons to play the game.
 function createButtons() {
-  // Your Code
+  startButton.parentNode.removeChild(startButton);
+
+
 }
 
 // Function to start the game by clearing the wrapper, creating
@@ -41,5 +59,6 @@ function startGame() {
   createCards();
 }
 
-// document.getElementById('start-game').addEventListener('click', startGame);
 startButton.addEventListener('click', startGame);
+// Event Listener will get startGame() method when button is clicked.
+// startGame() method will call createButtons() and createCards()
