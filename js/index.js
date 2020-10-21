332ea0dc148bc484d705e5b02b742dcd4c356c79
Peter Stevens <Peterstevens510@gmail.com>
2020-10-21 13:50:44 +0100

    show and hide button done

--- js/index.js
+++ js/index.js
@@ -1,18 +1,10 @@
 const suits = ['hearts', 'clubs', 'diamonds', 'spades'];
 const hand = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
-const cards = [];
-
 const cardsWrapper = document.querySelector('.cards-wrapper');
-
-const startButton = document.getElementById('start-game');
-
-const btnWrapper = document.querySelector('.btn-wrapper'); /* eslint-disable-line */
-const selectedCardsWrapper = document.querySelector('.selected-cards');
- /* eslint-disable-line */
-
 
 // Create an array with objects containing the value and the suit of each card (done)
 function createCards() {
+  const cards = [];
   for (let s = 0; s < suits.length; s += 1) {
     for (let c = 1; c <= hand.length; c += 1) {
       const cardObject = {
@@ -24,32 +16,45 @@ function createCards() {
   }
   // For each dataObject, create a new card and append it to the DOM
   cards.forEach((card, i) => {
-    const positionFromLeft = i * 25; // Card spacing
+    const positionFromLeft = i * 24; // Card spacing
     const cardElement = document.createElement('div');
-    cardElement.setAttribute('data-value', card.value);
+    cardElement.setAttribute('data-value', card.value); // Changes attribute class name.
     cardElement.classList.add('card', `${card.suit}-${card.value}`);
     cardElement.style.left = `${positionFromLeft}px`;
     cardsWrapper.append(cardElement);
   });
 }
 
-function shuffle() {
-
+function shuffleCards() {
+  for (let a = cards.length - 1; a > 0; a -= 1) {
+    const b = Math.floor(Math.random() * (b + 1));
+    [cards[a], cards[b]] = [cards[b], cards[a]];
+  }
 }
 
-function flipCards() {
-
+//look for 'hidden' among card classes, add if not present
+function showHideCards() {
+  return (cardsWrapper.classList.contains('hidden')) ? cardsWrapper.classList.remove('hidden') : cardsWrapper.classList.add('hidden');
 }
 
-function magic() {
+function magicSort() {
 
 }
-
 // Function to clear out the initial button and create new buttons to play the game.
 function createButtons() {
+  const startButton = document.getElementById('start-game');
   startButton.parentNode.removeChild(startButton);
+  const buttonWrapper = document.querySelector('.btn-wrapper');
 
+  // Creates show and hide button
+  const showAndHide = document.createElement('button');
+  showAndHide.setAttribute('id', 'show-hide') // Changes attribute class name.
+  showAndHide.setAttribute('class', 'btn btn-lg btn-secondary'); // Changes attribute class name.
+  showAndHide.innerHTML = 'Show/Hide';
+  showAndHide.addEventListener('click', showHideCards);
 
+  // Appends new buttons to buttonWrapper
+  buttonWrapper.appendChild(showAndHide);
 }
 
 // Function to start the game by clearing the wrapper, creating
@@ -59,6 +64,4 @@ function startGame() {
   createCards();
 }
 
-startButton.addEventListener('click', startGame);
-// Event Listener will get startGame() method when button is clicked.
-// startGame() method will call createButtons() and createCards()
+document.getElementById('start-game').addEventListener('click', startGame);
