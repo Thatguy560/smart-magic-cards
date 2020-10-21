9a8efa5f82d693ab1c2d8ed13e1707ad1f01a5dc
Peter Stevens <Peterstevens510@gmail.com>
2020-10-21 16:20:06 +0100

    shuffle method working using Rand method

--- js/index.js
+++ js/index.js
@@ -1,24 +1,26 @@
-const suits = ['hearts', 'clubs', 'diamonds', 'spades'];
+const suit = ['hearts', 'clubs', 'diamonds', 'spades'];
 const hand = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
+const cards = [];
+const cardSpacing = 25;
+
 const cardsWrapper = document.querySelector('.cards-wrapper');
 
 // Create an array with objects containing the value and the suit of each card (done)
 function createCards() {
-  const cards = [];
-  for (let s = 0; s < suits.length; s += 1) {
+  for (let s = 0; s < suit.length; s += 1) {
     for (let c = 1; c <= hand.length; c += 1) {
       const cardObject = {
         value: c,
-        suit: suits[s],
+        suit: suit[s],
       };
       cards.push(cardObject);
     }
   }
   // For each dataObject, create a new card and append it to the DOM
   cards.forEach((card, i) => {
-    const positionFromLeft = i * 24; // Card spacing
+    const positionFromLeft = i * cardSpacing; // Card spacing
     const cardElement = document.createElement('div');
-    cardElement.setAttribute('data-value', card.value); // Changes attribute class name.
+    cardElement.setAttribute('data-value', card.value); // Sets value of class name.
     cardElement.classList.add('card', `${card.suit}-${card.value}`);
     cardElement.style.left = `${positionFromLeft}px`;
     cardsWrapper.append(cardElement);
@@ -26,13 +28,18 @@ function createCards() {
 }
 
 function shuffleCards() {
-  for (let a = cards.length - 1; a > 0; a -= 1) {
-    const b = Math.floor(Math.random() * (b + 1));
-    [cards[a], cards[b]] = [cards[b], cards[a]];
-  }
+  // Math.random() returns a random number between 0 and 0.99
+  cards.sort(() => Math.random() - 0.5); // Randomly re-arranges the Cards array.
+  cards.forEach((card, i) => {
+    const positionFromLeft = i * cardSpacing; // Card spacing
+    const cardElement = document.createElement('div');
+    cardElement.setAttribute('data-value', card.value); // Sets value of class name.
+    cardElement.classList.add('card', `${card.suit}-${card.value}`);
+    cardElement.style.left = `${positionFromLeft}px`;
+    cardsWrapper.append(cardElement);
+  });
 }
-
-//look for 'hidden' among card classes, add if not present
+//look for 'hidden' among card classes, add if present remove it, else add it in.
 function showHideCards() {
   return (cardsWrapper.classList.contains('hidden')) ? cardsWrapper.classList.remove('hidden') : cardsWrapper.classList.add('hidden');
 }
@@ -40,25 +47,41 @@ function showHideCards() {
 function magicSort() {
 
 }
+
 // Function to clear out the initial button and create new buttons to play the game.
 function createButtons() {
   const startButton = document.getElementById('start-game');
   startButton.parentNode.removeChild(startButton);
   const buttonWrapper = document.querySelector('.btn-wrapper');
 
+  // Creates shuffle button
+  const shuffle = document.createElement('button'); // Creates new button Element.
+  // shuffle.setAttribute('id', 'shuffle'); // Sets value of class name.
+  shuffle.setAttribute('class', 'btn btn-lg btn-secondary'); // Sets value of class name.
+  shuffle.innerHTML = 'Shuffle'; // Text name of the button
+  shuffle.addEventListener('click', shuffleCards); // Calls shuffleCards method when button is clicked.
+
   // Creates show and hide button
-  const showAndHide = document.createElement('button');
-  showAndHide.setAttribute('id', 'show-hide') // Changes attribute class name.
-  showAndHide.setAttribute('class', 'btn btn-lg btn-secondary'); // Changes attribute class name.
-  showAndHide.innerHTML = 'Show/Hide';
-  showAndHide.addEventListener('click', showHideCards);
+  const showAndHide = document.createElement('button'); // Creates new button Element.
+  // showAndHide.setAttribute('id', 'show-hide'); // Sets value of class name.
+  showAndHide.setAttribute('class', 'btn btn-lg btn-secondary'); // Sets value of class name.
+  showAndHide.innerHTML = 'Show/Hide'; // Text name of the button
+  showAndHide.addEventListener('click', showHideCards); // Calls showHideCards method when button is clicked.
+
+  // Creates magic button
+  const magic = document.createElement('button'); // Creates new button Element.
+  // magic.setAttribute('id', 'magic'); // Sets value of class name.
+  magic.setAttribute('class', 'btn btn-lg btn-secondary'); // Sets value of class name.
+  magic.innerHTML = 'Magic'; // Text name of the button
+  magic.addEventListener('click', magicSort); // Calls showHideCards method when button is clicked.
 
   // Appends new buttons to buttonWrapper
+  buttonWrapper.appendChild(shuffle);
   buttonWrapper.appendChild(showAndHide);
+  buttonWrapper.appendChild(magic);
 }
 
-// Function to start the game by clearing the wrapper, creating
-// and appending the buttons and all the cards to the DOM
+// Function to start the game by clearing the wrapper, creating and appending the buttons and all the cards to the DOM
 function startGame() {
   createButtons();
   createCards();
